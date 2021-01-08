f45c826743da21d06ba6de7a0cf2a5d74ab2e3f4
Finbar16 <obichukwuma29@gmail.com>
2021-01-09 00:31:50 +0100

    started the react app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from "react";
 import "./style.css";
 
-function Nav(){
-  return(
+function Nav() {
+  return (
     <nav>
       <ul>
-        <li><a href="">Home</a></li>
-        <li><a href="">About</a></li>
-        <li><a href="">Contact</a></li>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
       </ul>
     </nav>
-  )
+  );
 }
-function Header(){
-  return(
+function Header() {
+  return (
     <header>
       <h1>Hello World</h1>
       <Nav />
     </header>
-  )
-
+  );
 }
 
 export default function App() {
-  return (
-    <Header />
-  );
+  return <Header />;
 }
